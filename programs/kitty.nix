f05061ccc870f399ce9bf7f002a2d98407678065
Rémi Nivet <rnivet@gmail.com>
2024-10-27 15:47:40 +0100

    Update Kitty config

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -11,12 +11,16 @@
     };
     themeFile = "tokyo_night_storm";
     settings = {
+      active_tab_background = "lightGreen";
+      color7 = "#ffffff";
+      foreground = "#ffffff";
+      hide_window_decorations = "titlebar-only";
+      selection_background = "#ffffff";
+      selection_foreground = "#414868";
       tab_bar_edge = "top";
       tab_bar_style = "powerline";
-      tab_bar_align = "center";
+      tab_bar_align = "left";
       tab_title_template = "{tab.active_wd}";
-      # foreground = "#ffffff";
-      color15 = "#ffffff";
     };
   };
 }

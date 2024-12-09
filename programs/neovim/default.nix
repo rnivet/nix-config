e16a4b0a17619ab0088b7fdd10a94b5fa4bd1414
Rémi Nivet <rnivet@gmail.com>
2024-12-09 15:19:26 +0100

    Install all treesitter grammars

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -1,34 +1,5 @@
 {pkgs, ...}: let
-  treesitterWithGrammars = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-    p.bash
-    p.comment
-    p.css
-    p.dockerfile
-    p.fish
-    p.gitattributes
-    p.gitignore
-    p.go
-    p.gomod
-    p.gowork
-    p.hcl
-    p.javascript
-    p.jq
-    p.json5
-    p.json
-    p.lua
-    p.make
-    p.markdown
-    p.nix
-    p.python
-    p.rust
-    p.toml
-    p.typescript
-    p.vue
-    p.yaml
-    p.regex
-    p.helm
-  ]);
-
+  treesitterWithGrammars = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
   treesitter-parsers = pkgs.symlinkJoin {
     name = "treesitter-parsers";
     paths = treesitterWithGrammars.dependencies;

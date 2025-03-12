f6d52cd7dca7067af94b8612d0f2caf1282cd534
Remi Nivet <remi.nivet@hornetsecurity.com>
2025-03-12 17:25:08 +0100

    Fix lspsaga

--- programs/neovim/nvim/lua/plugins/lsp/lspsaga.lua
+++ programs/neovim/nvim/lua/plugins/lsp/lspsaga.lua
@@ -6,7 +6,12 @@ return {
   },
   config = function()
     require("lspsaga").setup({})
+
     vim.keymap.set("n", "<leader>t", "<cmd>Lspsaga term_toggle<CR>",
       { desc = "Show Terminal", noremap = true, silent = true })
+
+    vim.diagnostic.config({
+      severity_sort = true,
+    })
   end
 }

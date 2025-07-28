38864dd0f7638ec02d3dcc7b65f67bbb0e0f9377
Rémi Nivet <rnivet@gmail.com>
2025-07-28 14:26:56 +0200

    Update codecompanion config

--- programs/neovim/nvim/lua/plugins/codecompanion.lua
+++ programs/neovim/nvim/lua/plugins/codecompanion.lua
@@ -5,40 +5,43 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  opts = {
-    adapters = {
-      codestral = function()
-        return require("codecompanion.adapters").extend("mistral", {
-          env = {
-            api_key = "CODESTRAL_API_KEY",
-            url = "https://codestral.mistral.ai",
-          },
-          schema = {
-            model = {
-              default = "codestral-latest"
+  config = function()
+    local opts = {
+      adapters = {
+        codestral = function()
+          return require("codecompanion.adapters").extend("mistral", {
+            env = {
+              api_key = "CODESTRAL_API_KEY",
+              url = "https://codestral.mistral.ai",
+            },
+            schema = {
+              model = {
+                default = "codestral-latest"
+              }
             }
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = "codestral"
+        },
+        inline = {
+          adapter = "codestral"
+        },
+        cmd = {
+          adapter = "codestral"
+        }
+      },
+      display = {
+        action_palette = {
+          opts = {
+            show_default_actions = true,
+            show_default_prompt_library = true,
           }
-        })
-      end,
-    },
-    strategies = {
-      chat = {
-        adapter = "codestral"
-      },
-      inline = {
-        adapter = "codestral"
-      },
-      cmd = {
-        adapter = "codestral"
-      }
-    },
-    display = {
-      action_palette = {
-        opts = {
-          show_default_actions = true,
-          show_default_prompt_library = true,
         }
       }
     }
-  }
+    require("codecompanion").setup(opts)
+  end
 }

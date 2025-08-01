b0def02b3df234347d37093ace01447fea936830
Remi Nivet <remi.nivet@hornetsecurity.com>
2025-08-01 17:26:08 +0200

    Activate agenix for work laptop

--- flake.nix
+++ flake.nix
@@ -70,7 +70,10 @@
 
       # Specify your home configuration modules here, for example,
       # the path to your home.nix.
-      modules = [./home.nix];
+      modules = [
+        agenix.homeManagerModules.default
+        ./home.nix
+      ];
 
       # Optionally use extraSpecialArgs
       # to pass through arguments to home.nix

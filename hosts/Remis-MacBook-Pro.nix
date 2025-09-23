9b7a8ffbcd719552f89f18acb49af3696f5429b0
Remi Nivet <rnivet@oversoc.com>
2025-09-23 10:15:52 +0200

    Install nodejs

--- hosts/Remis-MacBook-Pro.nix
+++ hosts/Remis-MacBook-Pro.nix
@@ -8,5 +8,6 @@
     import ../roles/common-dev.nix
     ++ import ../roles/common-desktop.nix
     ++ import ../roles/macos-dev.nix
-    ++ [../programs/dev-python.nix];
+    ++ [../programs/dev-python.nix]
+    ++ [../programs/dev-js.nix];
 }

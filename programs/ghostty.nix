eca357e3fa472826d7d142c28a0b2d8fa780f633
Remi Nivet <rnivet@oversoc.com>
2026-02-03 15:12:19 +0100

    fix ghostty keybinds

--- programs/ghostty.nix
+++ programs/ghostty.nix
@@ -7,15 +7,19 @@
     enable = true;
     package = pkgs.ghostty-bin;
     enableZshIntegration = true;
-    clearDefaultKeybinds = true;
     settings = {
       theme = "TokyoNight Moon";
-      macos-option-as-alt = true;
+      macos-option-as-alt = "left";
       font-family = "MesloLGS Nerd Font Mono";
       font-size = 14.5;
       working-directory = hostConf.homedir + "/projects";
       maximize = true;
       quit-after-last-window-closed = true;
+      keybind = [
+        "alt+t=unbind"
+        "alt+arrow_left=unbind"
+        "alt+arrow_right=unbind"
+      ];
     };
   };
 }

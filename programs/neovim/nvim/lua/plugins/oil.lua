e0d6b81f1743ed23aedcf6e111e35c0d7633817e
Remi Nivet <rnivet@oversoc.com>
2026-03-12 11:00:12 +0100

    show hidden files in nvim/oil

--- programs/neovim/nvim/lua/plugins/oil.lua
+++ programs/neovim/nvim/lua/plugins/oil.lua
@@ -12,6 +12,9 @@ return {
         padding = 5,
         max_width = 50,
       },
+      view_options = {
+        show_hidden = true
+      }
     })
     vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
   end
